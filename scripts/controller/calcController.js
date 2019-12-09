f29af0f6659dc29c24f5ac092779c16e8e637936
Flavio <fla_edu@live.com>
2019-12-08 21:56:19 -0300

    Eventos

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -10,6 +10,7 @@ class CalcController{
         this._displayCalc = "0";
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
     }
 
@@ -67,6 +68,31 @@ class CalcController{
     //#endregion
 
     //#region Events
-    
-    //#endregion
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach((btn, index)=>{
+
+            this.addEventListenerAll(btn, 'click drag', e=>{
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+                btn.style.cursor = 'pointer';
+            });
+        });
+
+    }
+
+    addEventListenerAll(element, events, fn){
+
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+
+    //#endregion 
 }
